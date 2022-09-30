ff5306856424e0a039f4efd86e92157f0f43b376
Zaki <omararturo.diazalarcon@upaep.edu.mx>
2022-09-30 10:06:42 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/correoelectronico/CorreoElectronico.java
@@ -0,0 +1,13 @@
+package com.example.correoelectronico;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CorreoElectronico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correo_electronico);
+    }
+}
