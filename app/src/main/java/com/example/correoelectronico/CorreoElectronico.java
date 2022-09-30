29c54dd5d12615c8063dc0eaef80aa87fa6a2819
Zaki <omararturo.diazalarcon@upaep.edu.mx>
2022-09-30 11:17:48 -0500

    Finished GUI Started on coding id's and Toast

--- app/src/main/java/com/example/correoelectronico/CorreoElectronico.java
+++ app/src/main/java/com/example/correoelectronico/CorreoElectronico.java
@@ -2,12 +2,26 @@ package com.example.correoelectronico;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CorreoElectronico extends AppCompatActivity {
+    // Variables
+    TextView txtRemitente, txtDestinatario, txtConCopia, txtAsunto, txtMensaje;
+
+    EditText edtEmailRemitente, edtEmailDestinatario, edtEmailConCopia, edtPlainAsunto, edtMultiMensaje;
+
+    Button btnAceptar, btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_correo_electronico);
+
+        // Texts
+        txtRemitente = findViewById(R.id.txtRemitente);
     }
+
+
 }
