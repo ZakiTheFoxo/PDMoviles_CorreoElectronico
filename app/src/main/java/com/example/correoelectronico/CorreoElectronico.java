f16806e6d403ae53f8b53d32f727a1e36d4ad90e
Zaki <omararturo.diazalarcon@upaep.edu.mx>
2022-10-14 11:20:25 -0500

    started working on intent to send mail

--- app/src/main/java/com/example/correoelectronico/CorreoElectronico.java
+++ app/src/main/java/com/example/correoelectronico/CorreoElectronico.java
@@ -1,6 +1,8 @@
 package com.example.correoelectronico;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,13 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CorreoElectronico extends AppCompatActivity implements View.OnClickListener{
+public class CorreoElectronico extends AppCompatActivity{
     // Variables
-    private TextView txtRemitente, txtDestinatario, txtConCopia, txtAsunto, txtMensaje;
+    private TextView    txtRemitente, txtDestinatario, txtConCopia, txtAsunto, txtMensaje;
 
-    private EditText edtEmailRemitente, edtEmailDestinatario, edtEmailConCopia, edtPlainAsunto, edtMultiMensaje;
+    private EditText    edtEmailRemitente, edtEmailDestinatario, edtEmailConCopia, edtPlainAsunto, edtMultiMensaje;
 
-    private Button btnAceptar, btnCancelar;
+    private Button      btnEnviar, btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,40 +25,51 @@ public class CorreoElectronico extends AppCompatActivity implements View.OnClick
 
         // Find on view by ID's
         // Texts
-        txtRemitente = findViewById(R.id.txtRemitente);
+        txtRemitente    = findViewById(R.id.txtRemitente);
         txtDestinatario = findViewById(R.id.txtDestinatario);
-        txtConCopia = findViewById(R.id.txtConCopia);
-        txtAsunto = findViewById(R.id.txtAsunto);
-        txtMensaje = findViewById(R.id.txtMensaje);
+        txtConCopia     = findViewById(R.id.txtConCopia);
+        txtAsunto       = findViewById(R.id.txtAsunto);
+        txtMensaje      = findViewById(R.id.txtMensaje);
 
         // TextFields
-        edtEmailRemitente = findViewById(R.id.edtEmailRemitente);
-        edtEmailDestinatario = findViewById(R.id.edtEmailDestinatario);
-        edtEmailConCopia =  findViewById(R.id.edtEmailConCopia);
-        edtPlainAsunto = findViewById(R.id.edtPlainAsunto);
-        edtMultiMensaje = findViewById(R.id.edtMultiMensaje);
+        edtEmailRemitente       = findViewById(R.id.edtEmailRemitente);
+        edtEmailDestinatario    = findViewById(R.id.edtEmailDestinatario);
+        edtEmailConCopia        = findViewById(R.id.edtEmailConCopia);
+        edtPlainAsunto          = findViewById(R.id.edtPlainAsunto);
+        edtMultiMensaje         = findViewById(R.id.edtMultiMensaje);
 
         // Buttons
-        btnAceptar = findViewById(R.id.btnAceptar);
+        btnEnviar   = findViewById(R.id.btnEnviar);
         btnCancelar = findViewById(R.id.btnCancelar);
 
         // Add listener to buttons
-        btnAceptar.setOnClickListener(this);
-        btnCancelar.setOnClickListener(this);
-    }
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Se presiono el boton Enviar", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.btnAceptar){
-            Toast.makeText(this, "Se presiono el boton Aceptar", Toast.LENGTH_SHORT).show();
-        }
-        if(view.getId() == R.id.btnCancelar){
-            edtEmailRemitente.setText("");
-            edtEmailDestinatario.setText("");
-            edtEmailConCopia.setText("");
-            edtPlainAsunto.setText("");
-            edtMultiMensaje.setText("");
-            Toast.makeText(this, "Se presiono el boton Cancelar", Toast.LENGTH_SHORT).show();
-        }
+                Intent intent   = new Intent();
+
+                intent.setAction(Intent.ACTION_SEND);
+
+                intent.putExtra(Intent.EXTRA_EMAIL, edtEmailRemitente.getText().toString());
+                intent.putExtra(Intent.EXTRA_EMAIL, edtEmailDestinatario.getText().toString());
+                intent.putExtra(Intent.EXTRA_CC, edtEmailConCopia.getText().toString());
+                intent.putExtra(Intent.EXTRA_SUBJECT, edtPlainAsunto.getText().toString());
+                intent.putExtra(Intent.EXTRA_TEXT, edtMultiMensaje.getText().toString());
+            }
+        });
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtEmailRemitente.setText("");
+                edtEmailDestinatario.setText("");
+                edtEmailConCopia.setText("");
+                edtPlainAsunto.setText("");
+                edtMultiMensaje.setText("");
+
+                Toast.makeText(getApplicationContext(), "Se presiono el boton Cancelar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
